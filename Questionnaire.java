59dd79da6f7dd1d3e6a4e296b9f9f60b12724480
Hendrik Werner <hendrik.to@gmail.com>
2016-03-06 02:23:56 +0100

    Add an interface for a questionnaire.

---
+++ Questionnaire.java
@@ -0,0 +1,21 @@
+package assignment5;
+
+/**
+ * An interface for a questionnaire.
+ *
+ * @author Hendrik Werner // s4549775
+ * @author Jasper Haasdijk // s4449754
+ */
+public interface Questionnaire {
+    /**
+     * Add a Vraag.
+     *
+     * @param v The Vraag to add.
+     */
+    public void add (Vraag v);
+
+    /**
+     * Start the questionnaire.
+     */
+    public void start ();
+}
