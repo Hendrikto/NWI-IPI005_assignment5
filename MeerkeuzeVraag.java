440e123b6c7dbbc455789ea7399bd4f9e3bd0ef4
Hendrikto <hendrik.to@gmail.com>
2016-11-04 21:36:35 +0100

    Format code.

--- MeerkeuzeVraag.java
+++ MeerkeuzeVraag.java
@@ -19,7 +19,7 @@ public class MeerkeuzeVraag extends Vraag {
      * @param answer The index of the right answer in the options argument
      * @param weight The weight of the question
      */
-    public MeerkeuzeVraag (String question, String[] options, int answer, int weight) {
+    public MeerkeuzeVraag(String question, String[] options, int answer, int weight) {
         super(question, getLetter(answer), weight);
         this.options = options;
     }
@@ -31,7 +31,7 @@ public class MeerkeuzeVraag extends Vraag {
      * @param options The answer options
      * @param answer The index of the right answer in the options argument
      */
-    public MeerkeuzeVraag (String question, String[] options, int answer) {
+    public MeerkeuzeVraag(String question, String[] options, int answer) {
         super(question, getLetter(answer));
         this.options = options;
     }
@@ -44,7 +44,7 @@ public class MeerkeuzeVraag extends Vraag {
      * @param answer The right answer
      * @param weight The weight of the question
      */
-    public MeerkeuzeVraag (String question, String[] options, String answer, int weight) {
+    public MeerkeuzeVraag(String question, String[] options, String answer, int weight) {
         super(question, answer, weight);
         this.options = options;
     }
@@ -56,7 +56,7 @@ public class MeerkeuzeVraag extends Vraag {
      * @param options The answer options
      * @param answer The right answer
      */
-    public MeerkeuzeVraag (String question, String[] options, String answer) {
+    public MeerkeuzeVraag(String question, String[] options, String answer) {
         super(question, answer);
         this.options = options;
     }
@@ -67,13 +67,18 @@ public class MeerkeuzeVraag extends Vraag {
      * @return a duplicate of this MeerkeuzeVraag
      */
     @Override
-    public MeerkeuzeVraag duplicate () {
+    public MeerkeuzeVraag duplicate() {
         int shift = new Random().nextInt(options.length - 1);
         String[] newOptions = new String[options.length];
         for (int i = 0; i < options.length; i++) {
             newOptions[(i + shift) % options.length] = options[i];
         }
-        return new MeerkeuzeVraag(text, newOptions, getLetter((solution.charAt(0) - 97 + shift) % options.length), weight);
+        return new MeerkeuzeVraag(
+                text,
+                newOptions,
+                getLetter((solution.charAt(0) - 97 + shift) % options.length),
+                weight
+        );
     }
 
     /**
@@ -82,11 +87,11 @@ public class MeerkeuzeVraag extends Vraag {
      * @return A String representation of this question
      */
     @Override
-    public String toString () {
+    public String toString() {
         StringBuilder sb = new StringBuilder();
         char index = 'a';
         sb.append(String.format("%s (weight: %d)\n", text, weight));
-        for (String s: options) {
+        for (String s : options) {
             sb.append(String.format("  %c) %s\n", index++, s));
         }
         return sb.toString();
@@ -98,7 +103,7 @@ public class MeerkeuzeVraag extends Vraag {
      * @param a The index as a number
      * @return The index as a letter
      */
-    private static String getLetter (int a) {
-        return String.valueOf((char)('a' + a));
+    private static String getLetter(int a) {
+        return String.valueOf((char) ('a' + a));
     }
 }
